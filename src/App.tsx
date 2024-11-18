35b5332948d2242bf96a8f063b8cf6688d0727eb
julianty <alexanderjulianty@gmail.com>
2024-11-18 15:51:29 -0800

    Add tabler icons and an improved landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import { IconShare3, IconSparkles, IconZoom } from "@tabler/icons-react";
 import { useState } from "react";
 import "./App.css";
 import beanCatalog from "./beanCatalog";
 import AddBeanForm from "./components/AddBeanForm";
-import DiscoverBeansSection from "./components/DiscoverBeansSection";
-import DiscoverRoastersSection from "./components/DiscoverRoastersSection";
 import HeroSection from "./components/HeroSection";
 import NavigationBar from "./components/NavigationBar";
 import UserBeansSection from "./components/UserBeansSection";
@@ -19,27 +18,110 @@ function App() {
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <NavigationBar />
-      {user ? (
-        <>
-          <UserProfile sectionStyle={sectionStyle} />
-          <section className={`${sectionStyle} flex gap-8`}>
-            <UserBeansSection beans={user.coffeeBeans} />
-            <div>
-              <h2>Add A New Bean</h2>
-              <AddBeanForm setBeanCatalog={setCurrentBeanCatalog} />
-            </div>
-          </section>
-        </>
-      ) : (
-        <HeroSection sectionStyle={sectionStyle} />
-      )}
-      <DiscoverBeansSection
-        sectionStyle={sectionStyle}
-        catalog={currentBeanCatalog}
-      />
-      <DiscoverRoastersSection sectionStyle={sectionStyle} />
+      <div className="flex flex-col items-center justify-center gap-12">
+        {user ? (
+          <>
+            <UserProfile sectionStyle={sectionStyle} />
+            <section className={`${sectionStyle} flex gap-8`}>
+              <UserBeansSection beans={user.coffeeBeans} />
+              <div>
+                <h2>Add A New Bean</h2>
+                <AddBeanForm setBeanCatalog={setCurrentBeanCatalog} />
+              </div>
+            </section>
+          </>
+        ) : (
+          <>
+            <HeroSection sectionStyle={sectionStyle} />
+            <FeaturesSection sectionStyle={sectionStyle} />
+            <FrequentAskedQuestionsSection sectionStyle={sectionStyle} />
+          </>
+        )}
+        {/* <DiscoverBeansSection
+          sectionStyle={sectionStyle}
+          catalog={currentBeanCatalog}
+        />
+        <DiscoverRoastersSection sectionStyle={sectionStyle} /> */}
+      </div>
     </UserContext.Provider>
   );
 }
 
+function FeaturesSection({ sectionStyle }: { sectionStyle: string }) {
+  function feature(
+    icon: JSX.Element,
+    title: string,
+    description: string
+  ): JSX.Element {
+    return (
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-2">
+          {icon}
+          <h3 className="font-bold">{title}</h3>
+        </div>
+        <p className="pl-6">{description}</p>
+      </div>
+    );
+  }
+  return (
+    <section className={`${sectionStyle} bg-slate-800`}>
+      <div className="flex flex-col gap-8">
+        {feature(
+          <IconSparkles />,
+          "Get Personalized Recommendations",
+          "Our recommendation system analyzes your preferences to suggest new beans to try"
+        )}
+
+        {feature(
+          <IconZoom />,
+          "Discover new coffee like never before",
+          "Search for beans by flavor profile, region, and more"
+        )}
+        {feature(
+          <IconShare3 />,
+          "Share your coffee journey with your friends",
+          "Find your community and share your favorite beans with them"
+        )}
+      </div>
+    </section>
+  );
+}
+
+function FrequentAskedQuestionsSection({
+  sectionStyle,
+}: {
+  sectionStyle: string;
+}) {
+  function question(question: string, answer: string): JSX.Element {
+    return (
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-2">
+          <h3 className="font-bold">{question}</h3>
+        </div>
+        <p className="pl-6">{answer}</p>
+      </div>
+    );
+  }
+  return (
+    <section className={`${sectionStyle}`}>
+      <h2>Frequently Asked Questions</h2>
+      <p> Can't find the answer you're looking for? Contact us.</p>
+      <div className="flex flex-col gap-8 mt-4">
+        {question(
+          "Is the service free to use?",
+          "Yes, Coffee Journal is free to use for all users"
+        )}
+
+        {question(
+          "Can I add coffee from an external site?",
+          "Yes, you can add coffee from any site by entering the details manually"
+        )}
+        {question(
+          "How are coffee recommendations generated?",
+          "Our recommendation system analyzes data from other coffee lovers and your own preferences to suggest new beans to try. We do not collect browsing data nor endorse roasters based on partnerships."
+        )}
+      </div>
+    </section>
+  );
+}
 export default App;
