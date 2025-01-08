fdfd50ed09c7d0f3ede0b8eea2666575bb51c058
julianty <alexanderjulianty@gmail.com>
2025-01-07 17:56:08 -0800

    Updates

--- src/components/UserContextProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { User } from "@/types";
-import React from "react";
-
-const UserContext = React.createContext<{
-  user?: User;
-  setUser?: React.Dispatch<React.SetStateAction<User | undefined>>;
-}>({});
-
-export default UserContext;
